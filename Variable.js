ae4627ec46025c8170a312429ce1245137d7a309
Amy Quinn <amyequinn@hotmail.co.uk>
2020-10-05 19:24:13 +0000

    Automatically backed up by Learn

---
+++ Variable.js
@@ -0,0 +1,18 @@
+
+var word = 'bird'
+console.log(word)
+
+word = 'dog'
+console.log(word)
+
+var a = 5, b = 2, c = 3, d = 'hello', e = 'goodbye'
+console.log(a,b,c)
+
+
+
+var myNote ="Get milk"
+
+myNote ="Get cookies"
+
+console.log(myNote)
+
